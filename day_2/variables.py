063af9de1de208b6ef39b9f395dd19dba344fb9d
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-27 02:25:09 +0300

    Day_2

---
+++ day_2/variables.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env  python3
+
+#Day 2: 30 Days of python programming
+
+first_name = "Erick"
+last_name = "Olando"
+full_name = "Erick Olando"
+country = "Kenya"
+city = "Nairobi"
+Age = 24
+year = 2017
+is_married = False
+is_true = "Loves coding"
+is_light_on = "No"
+
+#Declare multiple variable on one line
+name,age,vehicle = "Erick", 25, "BMW"
+
+
+
