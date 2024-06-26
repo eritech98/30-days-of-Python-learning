baeb8658ed829facc30fcb6e94862de40121d9f5
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-27 02:58:12 +0300

    Day_2

--- day_2/variables.py
+++ day_2/variables.py
@@ -16,5 +16,49 @@ is_light_on = "No"
 #Declare multiple variable on one line
 name,age,vehicle = "Erick", 25, "BMW"
 
+#Check the data type of all your variables using type() built-in function
+type(last_name)
+type(last-name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_true)
+type(is_light_on)
+type(is_married)
 
+
+#Using the len() built-in function, find the length of your first name
+len(first_name)
+
+#Compare the length of your first name and your last name
+len(first_name)
+len(last_name)
+
+
+#Declare 5 as num_one and 4 as num_two
+num_one = 5
+num_two = 4
+
+#Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
+
+#Subtract num_two from num_one and assign the value to a variable diff
+diff = num_one - num_two
+
+#Multiply num_two and num_one and assign the value to a variable product
+product = num_two * num_one
+
+#Divide num_one by num_two and assign the value to a variable division
+division = num_one/num_two
+
+#Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two % num_one
+
+#Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_one ** num_two
+
+#Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two
 
