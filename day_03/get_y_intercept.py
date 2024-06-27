4573f63610cb688f4435fce240d4d27463bf1114
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-28 01:10:59 +0300

    day_03

---
+++ day_03/get_y_intercept.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print()
+#Calculate the slope, x-intercept and y-intercept of y = 2x -2
+y = input("Input x equals to 0:")
+print()
+z = int(y)
+z  = 2  * int(0) - 2
+print("When x  equals to zero, y is", z)
+print()
+
