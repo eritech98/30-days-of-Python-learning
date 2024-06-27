8e6620261a67fbb71c98d2b216ae76362bc5faf9
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-27 22:34:06 +0300

    Enter side a: 5

---
+++ day_03/perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+Enter side a: 5
+Enter side b: 4
+Enter side c: 3
+The perimeter of the triangle is 12
+"""
+a = input("Enter side a: ")
+b = input("Enter side b: ")
+c = input("Enter side c: ")
+
+perimeter = int(a) + int(b) + int(c)
+
+print("The perimeter of the triangle is", perimeter)
+
+
+
