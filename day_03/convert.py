f25bf4d5c0a6c8d343e0062ab5d1df66d62279ea
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-30 21:33:13 +0300

    convert.py

---
+++ day_03/convert.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+#Find the length of the text python and convert the value to float and convert it to string
+
+
+a = str("python")
+
+b = len(a)
+
+c = float(b)
+d = str(c)
+print("The length of python string is ",b,".","we then convert it to float , which becomes",c,"Here is the data type after conversion.",type(c),"We then convert the float",c,"\n","to string",d,".","Here is the data type after conversion.",type(d))
+
+
