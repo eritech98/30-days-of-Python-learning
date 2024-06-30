1d4122202ded279a3f3fd822753311c663d8a10e
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-30 21:11:14 +0300

    check_on.py

---
+++ day_03/check_on.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#There is no 'on' in both dragon and python
+
+a = str("dragon")
+b = str("python")
+
+if "'on'" in a and b:
+    print("print 'on' is present in both dragon and python")
+else:
+    print("There is no 'on' in both dragon and python")
