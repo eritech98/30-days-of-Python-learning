8952b579ba9628a4ef517a329e5098ab95656c3e
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-30 19:21:37 +0300

    find_length_statement.py

---
+++ day_03/find_length_statement.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+#Find the length of 'python' and 'dragon' and make a falsy comparison statement
+
+a = str("python")
+b = str("dragon")
+
+print("Length of 'python': ",len(a))
+print("Length of 'dragon': ",len(b))
+
+print("Falsy statement: The length of 'python' string is not equal to the length of 'dragon' string")
+
+
