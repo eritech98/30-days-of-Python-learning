3de2183acc7aec97479f1670099fbc76ee38f0ca
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-06-30 20:47:23 +0300

    checking.py

---
+++ day_03/checking.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+
+#I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+
+a = str("I hope this course is not full of jargon")
+
+if "jargon" in a:
+    print("jargon  is in the sentence")
+else:
+    print("Jargon is not in the sentence")
+
