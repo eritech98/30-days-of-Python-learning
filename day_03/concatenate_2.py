f2754d9ea9e554c453ddb9a10dadf54a23dcace5
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-01 15:15:53 +0300

    concatenate_2.py

---
+++ day_03/concatenate_2.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+
+a = 'Coding'
+b = 'For'
+c = 'All'
+
+d = a +" "+ b +" "+c
+print(d)
