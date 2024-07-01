1221c931f22c93f879e166192c2dbe2b92d6687f
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-01 11:10:24 +0300

    table.py

---
+++ day_03/table.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+
+
+'''
+Write a Python script that displays the following table
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+
+'''
+print("",1,1,1,1,1,"\n",2,1,2,4,8,"\n",3,1,3,9,27,"\n",4,1,4,16,64,"\n",5,1,5,25,125,"\n")
+
+
+
+
