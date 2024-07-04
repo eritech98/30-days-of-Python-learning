5d52008b8a83ab118bfc2d0611e2c89aca0428c7
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-04 10:49:53 +0300

    concartenate.py

---
+++ day_04_strings/concartenate.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+'''
+Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+'''
+
+a = ['Thirty ','Days ','Of ','Python']
+
+print(''.join(a))
