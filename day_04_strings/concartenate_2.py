f8439c1a85a69805ad955e26aee5bc531693d892
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-04 10:57:23 +0300

    concartenate_2.py

---
+++ day_04_strings/concartenate_2.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+
+'''
+Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+'''
+
+strings = ['Coding ','For ','All','.']
+print(''.join(strings))
+
