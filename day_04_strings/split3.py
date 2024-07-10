1ad3915be1ced0ecf98f196fda92694e04cda848
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 18:58:18 +0300

    Split3_comma_method

---
+++ day_04_strings/split3.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+'''
+"Facebook, Google, Microsoft, Apple, IBM, Oracle,
+
+Amazon" split the string at the comma
+
+'''
+
+string = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+
+b = string.split(",")
+
+print(b)
