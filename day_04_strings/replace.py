208a2e9bb2f8a137efa703b8a7a874dc425848b0
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 18:35:54 +0300

    replace method

---
+++ day_04_strings/replace.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+
+'''
+Replace the word coding in the string 'Coding For All' to Python.
+
+'''
+
+string = 'Coding For All'
+
+c = string.replace("Coding","Python")
+
+print(c)
