aa7fa9edcf25505af8deff8fd4447366cd470608
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 18:27:19 +0300

    index

---
+++ day_04_strings/index.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+
+'''
+check if Coding For All string contains a word 
+
+Coding using the method index, find or other methods.
+
+'''
+
+strings = "Coding For All"
+c = "Coding"
+b = strings.index(c)
+
+print(b)
+
+
