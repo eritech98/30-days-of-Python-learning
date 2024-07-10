92c44c15dafc2fbef4f4d738f499def3a71c0472
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 18:26:06 +0300

    methods

---
+++ day_04_strings/methods.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+
+'''
+Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+
+'''
+string = "Coding For All"
+
+b = string.capitalize()
+print(b)
+c = string.title()
+print(c)
+d = string.swapcase()
+print(d)
+
