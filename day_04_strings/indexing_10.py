6d92ffe772ebfde9f065ac629f6fab57c1a5192d
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 19:18:20 +0300

    Recent changes

---
+++ day_04_strings/indexing_10.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+
+'''
+What character is at index 10 in "Coding For All" string.
+
+'''
+
+string = "Coding For All"
+b = string[10]
+print("The character is an empty space","'",b,"'")
+
---
+++ day_04_strings/last_index.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+
+'''
+What is the last index of the string Coding For All.
+
+'''
+string =  "Coding For All"
+
+print(len(string))
+
+print(string[13])
