1b3bcc44f86e7ad67886bc1e7706ce09aa7ee720
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 19:45:10 +0300

    python indexing_check

---
+++ day_04_strings/indexing_check.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+
+'''
+Use index to determine the position of 
+
+the first occurrence of C in Coding For All.
+
+'''
+
+
+
+def Erick():
+    string = "Coding For All"
+    if string[0] == 'C':
+        print("The position of the first occurrence of 'C' is index[0] ")
+    else:
+        print("The position of the first occurence of 'C' is another index number")
+Erick()
