e414a30748bed8838fae3c70b2a94f1ccdda6522
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 19:34:49 +0300

    Acronym2

---
+++ day_04_strings/acronym2.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python3
+
+'''
+Create an acronym or an abbreviation for 
+
+the name 'Coding For All'.
+
+'''
+
+string = "Coding For All"
+
+a = "Coding"
+d = a[0]
+b = "For"
+e = b[0]
+c = "All"
+f = c[0]
+
+print("The Acronym of","'",string,"'","is","'",d,".",e,".",f,"'")
+
