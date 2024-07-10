f0c9d5d78fc26f25fef1bffd5c1b1035f3a9e2b9
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 18:42:01 +0300

    replace_2

---
+++ day_04_strings/replace2.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+'''
+Change Python for Everyone to Python for All 
+
+using the replace method or other methods.
+
+'''
+
+string1 = "Python for Everyone"
+string2 = "Python for All"
+
+repla = string1.replace(string1,string2)
+print(repla)
