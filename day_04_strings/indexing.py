788a7468f9cf2f525977186a48df1a75acf4e096
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 19:03:27 +0300

    Indexing

---
+++ day_04_strings/indexing.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+'''
+What is the character at index 0 
+
+in the string Coding For All.
+
+'''
+
+string = "Coding For All"
+
+a = string[0]
+
+print("Its character ","'",a,"'")
