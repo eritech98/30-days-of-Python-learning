5c76576fc8c95dc5fe2825bee26062ecc938cb3e
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-10 19:26:45 +0300

    Abbreviation

---
+++ day_04_strings/acronym.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python3
+
+'''
+Create an acronym or an abbreviation for
+
+the name 'Python For Everyone'.
+
+'''
+
+string = "Python For Everyone"
+
+a = "Python"
+c = a[0]
+d = "For"
+e = d[0]
+f = "Everyone"
+g  = f[0]
+
+print("The acronym of","'",string,"'","is ","'",c,".",e,".",g,"'")
