e400dc0367d23f5f76d17bd0f49e53929977933d
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 14:29:35 +0300

    Does ''Coding For All' start with a substring Coding?

---
+++ day_04_strings/does_string.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+'''
+Does ''Coding For All' start with a substring Coding?
+
+'''
+
+a = "''Coding For All'"
+
+if a.find("Coding") == a[0]:
+    print("''Coding For All' starts with substring Coding")
+else:
+    print("''Coding For All' does not start with the Substring Coding")
