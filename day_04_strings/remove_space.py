a0d0d373c051fa3a7df92b36c53d201ddf97c08e
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 14:59:05 +0300

    remove_space

---
+++ day_04_strings/remove_space.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+
+'''
+'   Coding For All      '  , remove the left and right trailing spaces in the given string.
+
+'''
+
+string = '   Coding For All      '
+a = string.strip()
+print(a)
