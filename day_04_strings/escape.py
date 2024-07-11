534b1444d305fbe8470b4e3647cdd493f92ea41e
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 15:20:09 +0300

    escape sequence

---
+++ day_04_strings/escape.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+
+
+'''
+Use the new line escape sequence to separate the following sentences.
+I am enjoying this challenge.
+I just wonder what is next.
+'''
+print("\n","I am enjoying this challenge.","\n","\n","I just wonder what is next.")
