73e6d2911c6a6e2e1432faf2692c772786c6bbc0
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 15:28:43 +0300

    tab_space

---
+++ day_04_strings/tab_space.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+
+'''
+Use a tab escape sequence to write the following lines.
+
+Name      Age     Country   City
+Asabeneh  250     Finland   Helsinki
+
+'''
+
+print("\n","Name","\t\t","Age","\t","Country","\t","City","\n","Asabeneh","\t",250,"\t","Finland","\t","Helsinki")
+
