bef948c881cc2df31bb3011ec5ae7eb2fbbe5877
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 15:38:20 +0300

    string_formart

---
+++ day_04_strings/string_formart.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+
+'''
+
+Use the string formatting method to display the following:
+radius = 10
+area = 3.14 * radius ** 2
+The area of a circle with radius 10 is 314 meters square.
+
+'''
+
+def Erick():
+    radius = 10
+    area = 3.14 * radius ** 2
+    print('The area of a cicle with radius {} is {} meters square'.format(radius,area))
+Erick()
