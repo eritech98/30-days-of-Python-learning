255411c593bbfe856d85f5d887971c928283a6ab
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 14:54:45 +0300

    does_ending.py

---
+++ day_04_strings/does_ending.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+'''
+Does 'Coding For All' end with a substring coding?
+
+'''
+
+string = "'Coding For All'"
+
+if string.rfind("coding") == "coding":
+    print("'Coding For All' end with a substring coding")
+else:
+    print("'Coding For All' does not end with a substring coding")
+
