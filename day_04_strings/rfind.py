aa8ca02c606a09d2714e567a94dc74d5f8391a3f
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 12:46:14 +0300

    rfind_method

---
+++ day_04_strings/rfind.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+
+'''
+Use rfind to determine the position of the 
+
+last occurrence of l in Coding For All People.
+
+'''
+
+string = "Coding For All People"
+
+a = string.rfind("l")
+
+print(a)
+
