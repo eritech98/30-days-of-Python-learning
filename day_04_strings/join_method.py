6db8e650f6c4b1f36edde3f1939b524c1ad7aaaa
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-11 15:15:24 +0300

    python join_method.

---
+++ day_04_strings/join_method.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python3
+
+'''
+
+The following list contains the names of some of python libraries: 
+
+['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon'].
+
+Join the list with a hash with space string
+
+'''
+
+list = "['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon'] "
+
+a = "#"
+b = " "
+c = a +" "+ b
+
+d = "".join(c)
+
+print(list + d)
