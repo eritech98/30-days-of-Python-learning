795b393b93f65fd838bc3fa35d3cd18cf5e041af
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-12 16:02:11 +0300

    Adding 5 items in a list

--- day_05_Lists/declaration_list.py
+++ day_05_Lists/declaration_list.py
@@ -6,3 +6,12 @@ Declare a list with more than 5 items
 lista = ["Potato",4j,{"MY NAME IS ERICK OLANDO OPIYO",67},"Fourth item","Fifth item"]
 
 print(lista)
+#Find the length of your list
+print("The length of the list is",len(lista))
+
+#Get the first item, the middle item and the last item of the list
+
+first_item = lista[0]
+print(first_item)
+
+
