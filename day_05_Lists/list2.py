32ced55ce6d5cafb15def5100a07d08900e31822
Erick Opiyo Olando <Opiyoerick1998@gmail.com>
2024-07-12 16:08:20 +0300

    List2

---
+++ day_05_Lists/list2.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+
+'''
+Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+
+'''
+
+mixed_data_types = ["Erick Opiyo Olndo",26,176,"Not Married","P.O BOX 30100,ELDORET"]
+
+print(mixed_data_types)
---
+++ day_05_Lists/test.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+
+
+def Erick():
+    list1,list2,list3,list5 = input("Enter item 1(Number):"),input("Enter item 2(string):"),input("Enter item 3(complex):"),input("Enter item 5(float:")
+    list1a,listlb,listlc,listle = int(list1),str(list2),complex(list3),float(list5)
+    print('{} is an integer while {} is a string.Also,{} is a a  complex data type.Finally we have {} which is a float'.format(list1a,listlb,listlc,listle))
+Erick()
+
+
+
